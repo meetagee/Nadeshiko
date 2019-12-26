9a0db78ef6254ae1f4f7ae121950608f8f44210f
Anthon1e <anthonie3o4@gmail.com>
2019-12-26 02:49:57 -0800

    Adding graphing features

--- wolfram.py
+++ wolfram.py
@@ -12,9 +12,22 @@ class Wolfram(commands.Cog):
 
     @commands.command(pass_context=True)
     async def wolf(self, ctx, *, string):
-        """"""
+        """Wolframalpha solver, anything that normal wolframalpha can do"""
 
         globals.messages += 1
         result = client.query(string)
         answer = next(result.results).text
-        await ctx.send('Result: {}' .format(answer))
+        await ctx.send('{} Result: {}' .format(ctx.message.author.mention, answer))
+
+    @commands.command(pass_context=True)
+    async def graph(self, ctx, *, string):
+        """Wolframalpha graphing"""
+
+        globals.messages += 1
+        result = client.query("graph " + string)
+        #answer = next(result.results).subpods
+        result = list(result)
+        imageURL = result[1].get('subpod').get('img').get('@src')
+        embed = discord.Embed()
+        embed.set_image(url=imageURL)
+        await ctx.send(embed=embed)
